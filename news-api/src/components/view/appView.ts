04b6af0eb81710cf1fa308524ab172c7b4bab2da
anakdrch <ana.kudrevich@gmail.com>
2025-03-16 03:51:34 +0100

    refactor: add types in appView.ts

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,22 @@
-import News from './news/news';
-import Sources from './sources/sources';
+import News from './news/news.js';
+import Sources from './sources/sources.js';
+import { ResponseArticles } from '../interfaces/interfaces.js';
+import { ResponseSources } from '../interfaces/interfaces.js';
 
 export class AppView {
+    public news: News;
+    public sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: ResponseArticles) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: ResponseSources) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
