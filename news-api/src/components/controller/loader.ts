5053290879d33dc8b3e88b7d701c654699ce5976
anakdrch <ana.kudrevich@gmail.com>
2025-03-16 18:05:42 +0100

    refactor: add return type to errorHandler

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -22,7 +22,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res: Response) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
