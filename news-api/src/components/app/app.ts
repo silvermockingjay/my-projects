041547e0fbf10f776dcfcdfb3501f09fc59c3ed8
anakdrch <ana.kudrevich@gmail.com>
2025-03-16 10:46:49 +0100

    refactor: add types in app.ts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,16 +1,17 @@
-import AppController from '../controller/controller';
-import { AppView } from '../view/appView';
+import AppController from '../controller/controller.js';
+import { AppView } from '../view/appView.js';
 
 class App {
+    public controller: AppController;
+    public view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
     start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+        const sourcesBlock = document.querySelector('.sources') as HTMLDivElement;
+        sourcesBlock.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
