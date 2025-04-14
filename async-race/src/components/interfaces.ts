b8941ec0ca33e33398a00af5154c10fdcca66887
anakdrch <ana.kudrevich@gmail.com>
2025-04-14 10:31:48 +0200

    feat: implement Winner, AppState interfaces

--- async-race/src/components/interfaces.ts
+++ async-race/src/components/interfaces.ts
@@ -4,6 +4,23 @@ interface Car {
   id: number,
 }
 
+interface Winner {
+  id: number,
+  wins: number,
+  time: number,
+}
+
+export interface AppState {
+  view: string,
+  cars: Car[],
+  winners: Winner[],
+  garagePage: number,
+  winnersPage: number,
+  selectedId: string | null,
+  sortingOrder: 'asc' | 'desc',
+  sortBy: 'wins' | 'time',
+}
+
 export interface ButtonProps {
   type: 'button' | 'submit' | 'reset',
   text?: string,
