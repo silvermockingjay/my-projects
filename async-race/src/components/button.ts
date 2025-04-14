78b296143bb454c223247274410cca9c7fb00ad0
anakdrch <ana.kudrevich@gmail.com>
2025-04-14 09:37:08 +0200

    refactor: add return type of Button and type of btn for better readability

--- async-race/src/components/button.ts
+++ async-race/src/components/button.ts
@@ -1,7 +1,7 @@
 import type { ButtonProps } from "./interfaces";
 
-export const Button = ({type = 'button', text = '', onClick, className = ''}: ButtonProps) => {
-  const btn = document.createElement('button');
+export const Button = ({type = 'button', text = '', onClick, className = ''}: ButtonProps): HTMLButtonElement => {
+  const btn: HTMLButtonElement = document.createElement('button');
   btn.className = className;
   btn.type = type;
   btn.textContent = text;
