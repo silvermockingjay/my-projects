f61728fa821046753b3dd0e4b8a5d67402b394c8
anakdrch <ana.kudrevich@gmail.com>
2025-04-20 11:19:18 +0200

    feat: implement prevPage(), nextPage() functions

---
+++ async-race/src/routing/navigation.ts
@@ -0,0 +1,40 @@
+import { getState, setGaragePage } from "../state/states";
+
+export function prevPage(): void {
+  const nextPageBtn = document.querySelector('.nextButton') as HTMLButtonElement;
+  let currPage: number = 0;
+  const currView = getState('view');
+  if (currView === 'garage') {
+    currPage = getState('garagePage');
+  } else {
+    currPage = getState('winnersPage');
+  }
+  let prevPage: number = 0;
+  if (currPage > 1) {
+    prevPage = currPage - 1;
+    setGaragePage(prevPage);
+    if (nextPageBtn?.classList.contains('inactive')) nextPageBtn.classList.remove('inactive');
+  }
+}
+
+export function nextPage(): void {
+  const prevPageBtn = document.querySelector('.prevButton') as HTMLButtonElement;
+  const nextPageBtn = document.querySelector('.nextButton') as HTMLButtonElement;
+  let currPage: number = 0;
+  let totalPagesView: number = 0;
+  const currView = getState('view');
+  if (currView === 'garage') {
+    currPage = getState('garagePage');
+    totalPagesView = getState('totalPagesGarage');
+  } else {
+    currPage = getState('winnersPage');
+    totalPagesView = getState('totalPagesWinners');
+  }
+  let nextPage: number = 0;
+  if (currPage < totalPagesView) {
+    nextPage = currPage + 1;
+    setGaragePage(nextPage);
+    if (prevPageBtn?.classList.contains('inactive')) prevPageBtn.classList.remove('inactive');
+    if (nextPage === totalPagesView) nextPageBtn.classList.add('inactive');
+  }
+}
