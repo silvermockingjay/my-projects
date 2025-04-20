bdccd6e75f6b9b595a1443b6b6eca49bc618bed6
anakdrch <ana.kudrevich@gmail.com>
2025-04-20 17:12:23 +0200

    refactor: apply prettier for consistent code style

--- async-race/src/components/button.ts
+++ async-race/src/components/button.ts
@@ -1,15 +1,15 @@
-import type { ButtonProps } from "./interfaces";
+import type { ButtonProps } from './interfaces';
 
-export const button = ({type = 'button', text = '', onClick, className = ''}: ButtonProps): HTMLButtonElement => {
+export const button = ({ type = 'button', text = '', onClick, className = '' }: ButtonProps): HTMLButtonElement => {
   const btn: HTMLButtonElement = document.createElement('button');
   btn.className = className;
   btn.type = type;
   btn.textContent = text;
   if (onClick) {
-    btn.addEventListener('click', function(event) {
+    btn.addEventListener('click', function (event) {
       event.preventDefault();
       onClick();
     });
   }
   return btn;
-}
+};
