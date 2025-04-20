9f3b6d246fd7c32464074d5208ae5aa7208ec32b
anakdrch <ana.kudrevich@gmail.com>
2025-04-20 17:18:32 +0200

    refactor: apply prettier for a consistent code style

--- async-race/src/components/interfaces.ts
+++ async-race/src/components/interfaces.ts
@@ -1,72 +1,72 @@
 export interface Car {
-  name: string,
-  color: string,
-  id: number,
+  name: string;
+  color: string;
+  id: number;
 }
 
 export interface Winner {
-  id: number,
-  wins: number,
-  time: number,
+  id: number;
+  wins: number;
+  time: number;
 }
 
 export interface AppState {
-  view: string,
-  cars: Car[],
-  winners: Winner[],
-  garagePage: number,
-  winnersPage: number,
-  totalPagesGarage: number,
-  totalPagesWinners: number,
-  totalCars: number,
-  totalWinners: number,
-  limitCars: number,
-  limitWinners: number,
-  selectId: number | null,
-  removeId: number | null,
-  sortingOrder: 'asc' | 'desc',
-  sortBy: 'wins' | 'time',
+  view: string;
+  cars: Car[];
+  winners: Winner[];
+  garagePage: number;
+  winnersPage: number;
+  totalPagesGarage: number;
+  totalPagesWinners: number;
+  totalCars: number;
+  totalWinners: number;
+  limitCars: number;
+  limitWinners: number;
+  selectId: number | null;
+  removeId: number | null;
+  sortingOrder: 'asc' | 'desc';
+  sortBy: 'wins' | 'time';
   getState: <K extends keyof Omit<AppState, 'getState'>>(prop: K) => AppState[K];
-  setView: (view: string) => void,
-  setCars: (car: Car | Car[]) => void,
-  setUpdatedCar: (car: Car) => void,
-  setWinners: (winners: Winner | Winner[]) => void,
-  setTotal: (total: number, prop: 'cars' | 'winners') => void,
-  setTotalPagesGarage: () => void,
-  setTotalPagesWinners: () => void,
-  setGaragePage: (page: number) => void,
-  setWinnersPage: (page: number) => void,
-  setId: (id: number, type: 'select' | 'remove') => void,
-  setSortingOrder: (order: 'asc' | 'desc') => void,
-  setSortBy: (type: 'wins' | 'time') => void,
+  setView: (view: string) => void;
+  setCars: (car: Car | Car[]) => void;
+  setUpdatedCar: (car: Car) => void;
+  setWinners: (winners: Winner | Winner[]) => void;
+  setTotal: (total: number, prop: 'cars' | 'winners') => void;
+  setTotalPagesGarage: () => void;
+  setTotalPagesWinners: () => void;
+  setGaragePage: (page: number) => void;
+  setWinnersPage: (page: number) => void;
+  setId: (id: number, type: 'select' | 'remove') => void;
+  setSortingOrder: (order: 'asc' | 'desc') => void;
+  setSortBy: (type: 'wins' | 'time') => void;
 }
 
 export interface ButtonProps {
-  type: 'button' | 'submit' | 'reset',
-  text?: string,
-  onClick?: () => void,
-  className?: string,
+  type: 'button' | 'submit' | 'reset';
+  text?: string;
+  onClick?: () => void;
+  className?: string;
 }
 
 export interface InputProps {
-  type: string,
-  value?: string,
-  id?: string,
-  placeholder?: string,
-  disabled?: boolean,
-  className?: string,
+  type: string;
+  value?: string;
+  id?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  className?: string;
 }
 
 export interface FormProps {
-  formSubmitFun: (...args: string[]) => void,
-  inputFields: HTMLInputElement[],
-  formClass?: string,
-  btnClass?: string,
-  btnText: string,
+  formSubmitFun: (...args: string[]) => void;
+  inputFields: HTMLInputElement[];
+  formClass?: string;
+  btnClass?: string;
+  btnText: string;
 }
 
 export interface ListProps {
-  type: 'ul' | 'ol',
-  listItems?: HTMLLIElement[] | HTMLLIElement,
-  className?: string
+  type: 'ul' | 'ol';
+  listItems?: HTMLLIElement[] | HTMLLIElement;
+  className?: string;
 }
