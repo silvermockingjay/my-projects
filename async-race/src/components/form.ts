dc2e84e358d58cdca349122784aefa5d3bc0e9c7
anakdrch <ana.kudrevich@gmail.com>
2025-04-20 17:15:39 +0200

    refactor: apply prettier for a consistent code style

--- async-race/src/components/form.ts
+++ async-race/src/components/form.ts
@@ -1,31 +1,33 @@
-import type { FormProps, InputProps } from "./interfaces"
-import { button } from "./button";
+import type { FormProps, InputProps } from './interfaces';
+import { button } from './button';
 
-export const inputField = ({type, value, id, placeholder, disabled, className}: InputProps): HTMLInputElement => {
+export const inputField = ({ type, value, id, placeholder, disabled, className }: InputProps): HTMLInputElement => {
   const input: HTMLInputElement = document.createElement('input');
   input.type = type;
   if (placeholder) input.placeholder = placeholder;
   if (value) input.value = value;
-  if (id) input.id = id; 
+  if (id) input.id = id;
   if (disabled) input.disabled = disabled;
   if (className) input.className = className;
   return input;
-}
+};
 
-export const form = ({formClass, formSubmitFun, inputFields, btnClass, btnText,}: FormProps): HTMLFormElement => {
+export const form = ({ formClass, formSubmitFun, inputFields, btnClass, btnText }: FormProps): HTMLFormElement => {
   const formElem: HTMLFormElement = document.createElement('form');
   if (formClass) formElem.className = formClass;
 
-  inputFields.forEach((input) => formElem.append(input));
+  inputFields.forEach((input) => {
+    formElem.append(input);
+  });
 
-  const btn = button({type: 'submit', text: btnText});
+  const btn = button({ type: 'submit', text: btnText });
   if (btnClass) btn.className = btnClass;
   formElem.append(btn);
 
-  formElem.addEventListener('submit', function(event) {
+  formElem.addEventListener('submit', function (event) {
     event.preventDefault();
     const inputValues: string[] = inputFields.map((input) => input.value);
     formSubmitFun(...inputValues);
-  })
+  });
   return formElem;
-}
+};
